cbd21bfe68cfce9c70dd84361485d2de94854f0a
jpromanonet <programming@jpromano.net>
2020-06-09 11:47:43 -0600

    Half way there

---
+++ py_Toast_Notification.py
@@ -0,0 +1,10 @@
+# First of all, install the toast notification library this way:
+# python -m pip install win10toast
+
+#----------------------------------------------------------------
+
+# Now we import the toast library
+from win10toast from ToastNotifier
+
+# Now we initialize the toaster
+toaster = ToastNotifier()
