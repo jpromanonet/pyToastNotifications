8cdd7eb41e368bd1073311aac3ef7fd97942c73d
jpromanonet <programming@jpromano.net>
2020-06-09 11:54:29 -0600

    Done

--- py_Toast_Notification.py
+++ py_Toast_Notification.py
@@ -3,8 +3,25 @@
 
 #----------------------------------------------------------------
 
-# Now we import the toast library
-from win10toast from ToastNotifier
+# Now we import the toast library and time library
+from win10toast import ToastNotifier
+import time
 
 # Now we initialize the toaster
 toaster = ToastNotifier()
+
+# Show notifications every certain time
+
+# Declare notification variable time
+timeNotif = 2
+
+# Now we create the notification
+toaster.show_toast("Notification!",  # Title
+                    "This is an alert!!! Wiii!", # Body
+                    threaded=True,
+                    icon_path=None, # Icon
+                    duration = timeNotif )
+
+# Now we hold the notification there
+while toaster.notification_active():
+    time.sleep(0.3)
